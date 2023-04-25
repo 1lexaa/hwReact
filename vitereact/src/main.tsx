d977d975ead961fc40d1cd0db83b2a423d49a85b
gh0sty <nice140@icloud.com>
2023-04-25 21:33:38 +0300

    1

--- vitereact/src/main.tsx
+++ vitereact/src/main.tsx
@@ -2,8 +2,14 @@ import ReactDOM from "react-dom/client";
 import App  from "./App.tsx";
 import "./index.css";
 
+import Book from "./components/Book.tsx"
+import Music from "./components/Music.tsx";
+import Recipe from "./components/Recipe.tsx";
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <>
     <App />
+    <Book />
+    <Music />
+    <Recipe />
   </>
 );
